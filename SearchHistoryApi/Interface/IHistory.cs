b2fd0a5e42d295c3a6e4af679d4718204dba80fb
Natorden <71587437+Natorden@users.noreply.github.com>
2023-05-04 11:11:59 +0200

    added interface

---
+++ SearchHistoryApi/Interface/IHistory.cs
@@ -0,0 +1,13 @@
+﻿namespace SearchHistoryApi.Interface;
+
+public interface IHistory
+{
+    //This method call should retrieve a list of all searches
+    //that have been done, as long as the application is running
+    void GetHistory();
+
+    //This method call should save each search done, and could
+    //optionally include more data, retrieved from each search
+    //such as time taken, and results
+    void SaveHistory(string s);
+}
