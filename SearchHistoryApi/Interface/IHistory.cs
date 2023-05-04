0dde87670abc7a9e33c32a616b19fe818cd79ef8
Natorden <71587437+Natorden@users.noreply.github.com>
2023-05-04 13:15:01 +0200

    interface fix

--- SearchHistoryApi/Interface/IHistory.cs
+++ SearchHistoryApi/Interface/IHistory.cs
@@ -4,7 +4,7 @@ public interface IHistory
 {
     //This method call should retrieve a list of all searches
     //that have been done, as long as the application is running
-    void GetHistory();
+    List<String> GetHistory();
 
     //This method call should save each search done, and could
     //optionally include more data, retrieved from each search
